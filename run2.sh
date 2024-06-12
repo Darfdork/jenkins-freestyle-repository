c6c0dcb0004211f5a14f4649a41ef7f947dad350
AGRiff <darfdork@googlemail.com>
2024-06-12 14:27:26 +0100

    changed file

--- run2.sh
+++ run2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Hello! This is Adam Welcome to our GITHUB!"
-
+echo "Hello from the Jenkins job named: ${JOB_NAME}"
+touch 1.txt 2.txt 3.txt 4.txt 5.txt
+sudo apt-get install zip -y
+zip archive.zip *.txt
