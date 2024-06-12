11b936e05e906e32d978542677d8e7f230e01041
AGRiff <darfdork@googlemail.com>
2024-06-12 17:04:51 +0100

    test

--- run2.sh
+++ run2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello! This is Adam Welcome to our GITHUB! via JENKINS! with a SECRET!"
+echo "Hello! This is Adam Welcome to our GITHUB! via JENKINS! with a KEY!"
 echo "Hello from the Jenkins job named: ${JOB_NAME}"
 touch 1.txt 2.txt 3.txt 4.txt 5.txt
 zip archive.zip *.txt
